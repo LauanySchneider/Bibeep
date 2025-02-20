edb578c658542c8a27252e4b9c98d2161d2825af
Murilo Garcia <148296967+MuriloGRibeiro@users.noreply.github.com>
2025-02-20 09:15:32 -0300

    Update web.php
    Alterado para abrir direto ao invés do HOME

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutosController;
 
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('index');  // Página de apresentação
 })->name('home');
 
